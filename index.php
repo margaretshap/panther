a9d2a8b1fa27f82106fca3ca727a82618fda38dd
margaretshap <45239487+margaretshap@users.noreply.github.com>
2021-11-20 16:40:35 +0100

    Add files via upload
    tryy

---
+++ index.php
@@ -0,0 +1,493 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome on Panther Adventures</title>
+     <link href="fontawesome-free-5.15.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+   <script defer src="fontawesome-free-5.15.1-web/js/all.js"></script> <!--load all styles -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<script src="bootstrap/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="bootstrap/dist/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+   <link rel="stylesheet" type="text/css"href="style.css">
+
+    <script src="https://kit.fontawesome.com/840f1100f9.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta name="theme-color" content="#00A170">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.min.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="assets/css/media-queries.css">
+        <meta name="viewport" content="width=device-width">
+        <meta name="color-scheme" content="dark light">  
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<script src="bootstrap/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+<script type="text/javascript">
+ 
+</script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+  @import url(https://fonts.googleapis.com/css?family=Roboto:400,100,900);
+
+html,
+body {
+  -moz-box-sizing: border-box;
+       box-sizing: border-box;
+  height: 100%;
+  width: 100%; 
+  background: #FFF;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+}
+ 
+.wrapper {
+  display: table;
+  height: 100%;
+  width: 100%;
+}
+
+.container-fostrap {
+  display: table-cell;
+  padding: 1em;
+  text-align: center;
+  vertical-align: middle;
+}
+.fostrap-logo {
+  width: 100px;
+  margin-bottom:15px
+}
+h1.heading {
+  color: #fff;
+  font-size: 1.15em;
+  font-weight: 900;
+  margin: 0 0 0.5em;
+  color: #505050;
+}
+@media (min-width: 450px) {
+  h1.heading {
+    font-size: 3.55em;
+  }
+}
+@media (min-width: 760px) {
+  h1.heading {
+    font-size: 3.05em;
+  }
+}
+@media (min-width: 900px) {
+  h1.heading {
+    font-size: 3.25em;
+    margin: 0 0 0.3em;
+  }
+} 
+.card {
+  display: block; 
+    margin-bottom: 20px;
+    line-height: 1.42857143;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12); 
+    transition: box-shadow .25s; 
+}
+.card:hover {
+  box-shadow: 0 8px 17px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+}
+.img-card {
+  width: 100%;
+  height:200px;
+  border-top-left-radius:2px;
+  border-top-right-radius:2px;
+  display:block;
+    overflow: hidden;
+}
+.img-card img{
+  width: 100%;
+  height: 200px;
+  object-fit:cover; 
+  transition: all .25s ease;
+} 
+.card-content {
+  padding:15px;
+  text-align:left;
+}
+.card-title {
+  margin-top:0px;
+  font-weight: 700;
+  font-size: 1.65em;
+}
+.card-title a {
+  color: #000;
+  text-decoration: none !important;
+}
+.card-read-more {
+  border-top: 1px solid #D4D4D4;
+}
+.card-read-more a {
+  text-decoration: none !important;
+  padding:10px;
+  font-weight:600;
+  text-transform: uppercase
+}
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <!-- Brand/logo -->
+  <!-- <a class="navbar-brand" href="#">Logo</a>-->
+   <img src="Panther.png" alt="Logo" style="width:120px;">
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Tour Packages</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Inclusive Tourism</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Car Hire</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Special Offers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Air Ticketing+Hotel</a>
+    </li>
+  </ul>
+  <button type="button" class="btn btn-primary" style="margin-left: 350px;">Book Now</button>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="crop.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Rwanda Akagera</h3>
+        <p>We had such a great time with panther adventures  </p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="crop.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Rubavu</h3>
+        <p>Thank you, </p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="crop.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Karongi</h3>
+        <p>We love the Big </p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<div class="row">
+  <div class="col" style="margin-left: 50px;margin-top: 30px;">
+<h2 style="color: green;">Panther Adventures Ltd</h2>
+    <p><b>Panther Adventures Ltd</b> is a Rwandan tour and travel social enterprise intends to provide individual and group travel services. And in return support people with disabilities.</p>
+    <P>At Panther Adventures, we are redefining the car rental & tourism industry
+with our unique, bespoke & premium offerings. We believe that your
+business or holiday trip should be as stress free as possible and armed with
+expert local knowledge of the Rwandan car rental and tours industry,
+Panther Adventures guarantees you a journey as great as the destinations
+you visit.</P>
+<P>Panther Adventures provide warm and personal hospitality to all our guests
+to allow for maximum enjoyment of the richness of Rwanda and the region,
+providing an agile, personalized and efficient service, with the backing of
+professionals of vast experience in the field of transport and tourism.</P>
+     </div>
+  <div class="col"> 
+
+<!-- 21:9 aspect ratio -->
+<div class="embed-responsive embed-responsive-16by9" style="margin-top:30px;">
+  <iframe class="embed-responsive-item" src="MAG.mp4"></iframe>
+</div>
+
+
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-3"></div>
+  <div class="col-sm-5" style="margin-top:20px; margin-left: 40px;">
+<h5 style="margin-left:90px;color:#ff8000">Create Your Own adventure</h5>
+<h6>Discover Rwanda Discover your self with Panther Adventures </h6>
+  </div>
+  <div class="col-sm-3"></div>
+</div>
+
+<section class="wrapper" style="margin-top: -150;">
+    <div class="container-fostrap">
+        <div>
+            
+            <h1 class="heading">
+                Choose the best Packages.
+            </h1>
+        </div>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html">
+                            <img src="vb.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html"> Travel
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    Tutorial to make a carousel bootstrap by adding more wonderful effect fadein ...
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb2.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/02/awesome-material-design-responsive-menu.html"> Trip
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    Material Design is a visual programming language made by Google. Language 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                       <div class="col-xs-12 col-sm-4">
+                        <div class="card">
+                            <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                            <img src="vb3.jpeg" />
+                          </a>
+                            <div class="card-content">
+                                <h4 class="card-title">
+                                    <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">Visit
+                                  </a>
+                                </h4>
+                                <p class="">
+                                    tutorials button hover animation, although very much a hover button is very 
+                                </p>
+                            </div>
+                            <div class="card-read-more">
+                                <button type="button" class="btn btn-success">Book</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
